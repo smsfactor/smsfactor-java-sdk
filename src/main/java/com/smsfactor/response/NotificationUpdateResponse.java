f31d74465170ec48f2b1b7e681e67b7d56cdcdea
mathieu <mathieu.p@smsfactor.com>
2019-03-27 09:56:54 +0100

    Notification update

--- src/main/java/com/smsfactor/response/NotificationUpdateResponse.java
+++ src/main/java/com/smsfactor/response/NotificationUpdateResponse.java
@@ -1,5 +1,14 @@
 package com.smsfactor.response;
 
 public class NotificationUpdateResponse extends NotificationGetReponse {
+    public class Notification {
 
+        public String alert_email;
+        public String alert_gsm;
+        public String email;
+        public String phone_number;
+        public String alert_trigger;
+    }
+
+    public Notification notification;
 }

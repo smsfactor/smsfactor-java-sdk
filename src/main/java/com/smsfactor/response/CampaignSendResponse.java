dccec40468a1e4ef7cabb7689acc6c36b3b76ee7
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2020-05-04 21:34:40 +0200

    Fix: ticket type is a string (Fix #1)

--- src/main/java/com/smsfactor/response/CampaignSendResponse.java
+++ src/main/java/com/smsfactor/response/CampaignSendResponse.java
@@ -2,7 +2,7 @@ package com.smsfactor.response;
 
 public class CampaignSendResponse extends SMSFactorResponse
 {
-	public Integer ticket;
+	public String ticket;
 	public Integer cost;
 	public Integer credits;
 	public Integer total;
